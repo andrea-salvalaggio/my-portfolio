a0caa8e41efea54790289edeefe3f5b3b1b5d1dd
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-25 16:07:10 +0100

    feat: :sparkles: Add elements to hero component

--- src/components/Header/Hero/HeroElement.vue
+++ src/components/Header/Hero/HeroElement.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    
+  <div class="col-12 d-flex flex-column align-items-center justify-content-center">
+    <div class="profile-container">
+      <img src="../../../assets/img/profile-pic-light.png" alt="Profile Image">
+    </div>
+    <div class="title-container text-center py-3">
+      <h2>Hey! I’m Andrea<br>Full Stack Web Developer</br> Based in Padua, Italy
+      </h2>
+    </div>
+    <div class="d-md-flex">
+      <div class="col-12">
+        bottone 1
+      </div>
+      <div class="col-12">
+        bottone 2
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +24,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../../../../styles/variables.scss";
 
+.profile-container{
+  width: 120px;
+
+  img{
+    width: 100%;
+    height: 100%;
+  }
+}
+
+h2{
+  font-size: $h2FontSize;
+  font-weight: 800;
+}
 </style>
