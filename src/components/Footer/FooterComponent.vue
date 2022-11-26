f7f7b6e564c4e6ac9f878eb32f4023472b467afb
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-26 18:24:20 +0100

    style: :art: Finish style the footer

--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="container px-lg-5">
-    <div class="row px-md-4 px-lg-5">
-      <div class="col-12 d-flex justify-content-between align-items-center py-4 px-5">
-        <div class="icon">
-          <i class="ri-github-fill"></i>
-          <i class="ri-linkedin-fill ps-3"></i>
-        </div>
-        <div>
-          <p class="copyright">© Andrea Salvalaggio <span class="ps-1">2022</span></p>
+  <div class="container-fluid p-0 m-0">
+    <hr class="stroke-line m-0">
+    <div class="container px-lg-5">
+      <div class="row px-md-4 px-lg-5">
+        <div class="col-12 d-flex justify-content-between align-items-center py-4 px-5">
+          <div class="icon">
+            <i class="ri-github-fill fs-5"></i>
+            <i class="ri-linkedin-fill fs-5 ps-3"></i>
+          </div>
+          <div>
+            <p class="copyright m-0">© Andrea Salvalaggio <span class="ps-1">2022</span></p>
+          </div>
         </div>
       </div>
     </div>
@@ -24,7 +27,7 @@ export default {
 @import "../../../styles/variables.scss";
 
 .icon {
-  &:hover {
+  i:hover{
     color: $primaryColorBrand;
     cursor: pointer;
   }
@@ -37,5 +40,9 @@ export default {
   span{
     color: $primaryColorBrand;
   }
+}
+
+.stroke-line{
+  border: 2px solid $secondaryColorBgLight;
 }
 </style>
