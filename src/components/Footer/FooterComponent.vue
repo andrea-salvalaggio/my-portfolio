323bbf9d777f18a48fd654a67b6cca6978cdb647
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-26 18:14:14 +0100

    feat: :sparkles: Start creating the footer section

--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    Footer
+  <div class="container px-lg-5">
+    <div class="row px-md-4 px-lg-5">
+      <div class="col-12 d-flex justify-content-between align-items-center py-4 px-5">
+        <div class="icon">
+          <i class="ri-github-fill"></i>
+          <i class="ri-linkedin-fill ps-3"></i>
+        </div>
+        <div>
+          <p class="copyright">© Andrea Salvalaggio <span class="ps-1">2022</span></p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +20,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../../../styles/variables.scss";
 
+.icon {
+  &:hover {
+    color: $primaryColorBrand;
+    cursor: pointer;
+  }
+}
+
+.copyright{
+  font-size: $smallFontSize;
+  font-weight: $smallFontWeight;
+
+  span{
+    color: $primaryColorBrand;
+  }
+}
 </style>
