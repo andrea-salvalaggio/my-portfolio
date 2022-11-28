3d62199305fb2c510ad9e39b8692fd16e55c3d36
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-28 17:32:54 +0100

    fix: :art: Fix padding pill on nav in main component

--- src/components/Main/MainComponent.vue
+++ src/components/Main/MainComponent.vue
@@ -2,7 +2,7 @@
   <div class="container px-lg-5 big-margin-top">
     <div class="row justify-content-center mx-3 mx-md-0">
       <div class="col-12 col-md-8 col-lg-6 nav-container d-flex px-2 py-2">
-        <div class="col-6 btn-container pe-2">
+        <div class="col-6 btn-container">
           <button class="btn text-uppercase border-0 px-4 py-3">
             <a class="text-decoration-none">projects
             </a>
