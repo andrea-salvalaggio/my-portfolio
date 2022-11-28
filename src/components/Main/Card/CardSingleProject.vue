febe5a5f599cedef28c81901f2bed5ba0342ed68
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-28 15:41:41 +0100

    feat: :sparkles: Start creating overlay project effect

--- src/components/Main/Card/CardSingleProject.vue
+++ src/components/Main/Card/CardSingleProject.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="d-md-flex flex-md-wrap my-5 p-0">
-        <div v-for="(project, index) in projects" :key="index" class="col-12 col-md-6 project-container">
+    <div class="d-md-flex flex-md-wrap justify-content-center my-5 p-0">
+        <div v-for="(project, index) in projects" :key="index" class="col-12 col-md-6 col-lg-5 project-container">
+            <div class="overlay-container">
+                <div class="">
+                    <p>{{ project.type }}</p>
+                    <h2>{{ project.title }}</h2>
+                    <button>{{ project.cta }}</button>
+                </div>
+            </div>
             <img :src="project.img" :alt="project.text" class="p-3">
         </div>
     </div>
@@ -13,7 +20,10 @@ export default {
             projects:[
                 {
                     img: require("../../../assets/img/projects/deliveboo.png"),
-                    text: "DeliveBoo Project"
+                    text: "DeliveBoo Project",
+                    type: "prova",
+                    title: "Deliveboo",
+                    cta: "view on github"
                 },
                 {
                     img: require("../../../assets/img/projects/boolzapp.png"),
@@ -37,6 +47,7 @@ export default {
 @import "../../../../styles/variables.scss";
 
 .project-container {
+    position: relative;
 
     img {
         width: 100%;
@@ -44,10 +55,13 @@ export default {
     }
 }
 
-// @media screen and (min-width: 768px) {
-//     .project-container{
-//         width: calc(100% / 2);
-//     }
-// }
+.overlay-container{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background-color: $primaryColorOverlay;
+}
 
 </style>
