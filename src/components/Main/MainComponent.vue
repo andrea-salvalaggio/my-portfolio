717afd7676c39f2d4e5951bd07fea8c9e86118c6
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-28 09:18:06 +0100

    style: :art: Fix border radius of nav container

--- src/components/Main/MainComponent.vue
+++ src/components/Main/MainComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container px-lg-5 mt-5">
     <div class="row justify-content-center mx-3 mx-md-0">
-      <div class="col-12 col-md-8 col-lg-6 d-flex py-2 bg-dark">
+      <div class="col-12 col-md-8 col-lg-6 nav-container d-flex px-2 py-2 bg-dark">
         <div class="col-6 btn-container pe-2">
           <button class="btn text-uppercase text-white border-0 px-4 py-3">projects</button>
         </div>
@@ -21,6 +21,10 @@ export default {
 
 <style lang="scss" scoped>
 @import "../../../styles/variables.scss";
+
+.nav-container{
+  border-radius: $navRadius;
+}
 
 .btn-container {
   button {
