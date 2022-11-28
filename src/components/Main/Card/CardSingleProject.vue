dc89ef94dfdf8d9243eb8b5386f59993c9ca5c53
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-28 16:45:49 +0100

    style: :art: Add more style to overlay effect card

--- src/components/Main/Card/CardSingleProject.vue
+++ src/components/Main/Card/CardSingleProject.vue
@@ -4,8 +4,13 @@
             <div class="overlay-container position-absolute top-50 start-50 translate-middle">
                 <div class="overlay-description">
                     <p class="text-uppercase m-0">{{ project.type }}</p>
-                    <h2>{{ project.title }}</h2>
-                    <button>{{ project.cta }}</button>
+                    <h2 class="pb-3">{{ project.title }}</h2>
+                    <button class="btn text-uppercase text-white border-0 px-4 py-3">
+                        <a class="text-decoration-none" :href="project.link" target="_blank">
+                        {{ project.cta }}
+                        <i class="ri-arrow-right-up-line align-top ps-1"></i>
+                        </a>
+                    </button>
                 </div>
             </div>
             <img :src="project.img" :alt="project.text" class="p-3">
@@ -21,21 +26,34 @@ export default {
                 {
                     img: require("../../../assets/img/projects/deliveboo.png"),
                     text: "DeliveBoo Project",
-                    type: "prova",
+                    type: "web app – ui/ux design",
                     title: "Deliveboo",
-                    cta: "view on github"
+                    cta: "view on github",
+                    link: "https://github.com/andrea-salvalaggio/laravel-deliveboo"
                 },
                 {
                     img: require("../../../assets/img/projects/boolzapp.png"),
-                    text: "Boolzapp Project"
+                    text: "Boolzapp Project",
+                    type: "web app",
+                    title: "Boolzapp",
+                    cta: "view on github",
+                    link: "https://github.com/andrea-salvalaggio/vue-boolzapp"
                 },
                 {
                     img: require("../../../assets/img/projects/discord.png"),
-                    text: "Discord Project"
+                    text: "Discord Project",
+                    type: "web design",
+                    title: "Discord",
+                    cta: "view on github",
+                    link: "https://github.com/andrea-salvalaggio/htmlcss-discord"
                 },
                 {
                     img: require("../../../assets/img/projects/spotify.png"),
-                    text: "Spotify Project"
+                    text: "Spotify Project",
+                    type: "web app",
+                    title: "Spotify",
+                    cta: "view on github",
+                    link: "https://github.com/andrea-salvalaggio/html-css-spotifyweb"
                 },
             ]
         }
@@ -50,24 +68,6 @@ export default {
     position: relative;
     overflow: hidden;
 
-    .overlay-description{
-        position: absolute;
-        bottom: 0;
-        padding: 2rem;
-
-        p{
-            font-size: $normalFontSize;
-            font-weight: $smallFontWeight;
-            color: $secondryColorBrand;
-        }
-
-        h2{
-            font-size: $h2FontSize;
-            font-weight: $bigFontWeight;
-            color: $secondaryColorText;
-        }
-    }
-
     img {
         width: 100%;
         height: 100%;
@@ -80,6 +80,35 @@ export default {
     height: calc(100% - 1.9rem);
     background-color: $primaryColorOverlay;
     border-radius: $overlayRadius;
+
+    .overlay-description{
+        position: absolute;
+        bottom: 0;
+        padding: 2rem;
+
+        p{
+            font-size: $normalFontSize;
+            font-weight: $normalFontWeight;
+            color: $secondryColorBrand;
+        }
+
+        h2{
+            font-size: $h2FontSize;
+            font-weight: $bigFontWeight;
+            color: $secondaryColorText;
+        }
+
+        button {
+            font-size: $normalFontSize;
+            font-weight: $smallFontWeight;
+            border-radius: $btnRadius;
+            background-color: $secondryColorBrand;
+
+            a{
+                color: $primaryColorBrand;
+            }
+        }
+    }
 }
 
 </style>
