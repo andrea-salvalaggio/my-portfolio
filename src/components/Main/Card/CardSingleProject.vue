e1ad5c024ca1c0491e999c9d95cdf9df18b5d37e
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-28 12:36:16 +0100

    fix: :sparkles: Fix projects container from mobile to desktop view

--- src/components/Main/Card/CardSingleProject.vue
+++ src/components/Main/Card/CardSingleProject.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="col-12 d-flex flex-wrap project-container my-5">
-        <img v-for="(project, index) in projects" :key="index" class="w-50 p-3" :src="project.img" :alt="project.text">
+    <div class="d-md-flex flex-md-wrap my-5 p-0">
+        <div v-for="(project, index) in projects" :key="index" class="col-12 col-md-6 project-container">
+            <img :src="project.img" :alt="project.text" class="p-3">
+        </div>
     </div>
 </template>
 
@@ -35,9 +37,17 @@ export default {
 @import "../../../../styles/variables.scss";
 
 .project-container {
-  img {
-    border-radius: $projectRadius;
-  }
+
+    img {
+        width: 100%;
+        border-radius: $projectRadius;
+    }
 }
+
+// @media screen and (min-width: 768px) {
+//     .project-container{
+//         width: calc(100% / 2);
+//     }
+// }
 
 </style>
