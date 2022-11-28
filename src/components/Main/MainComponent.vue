af85cc81a14b0e1ff5a7bb52a487a631c0b0b9f0
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-28 09:31:54 +0100

    fix: :sparkles: Add margin to main containier section

--- src/components/Main/MainComponent.vue
+++ src/components/Main/MainComponent.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="container px-lg-5 mt-5">
+  <div class="container px-lg-5 big-margin-top">
     <div class="row justify-content-center mx-3 mx-md-0">
-      <div class="col-12 col-md-8 col-lg-6 nav-container d-flex px-2 py-2 bg-dark">
+      <div class="col-12 col-md-8 col-lg-6 nav-container d-flex px-2 py-2">
         <div class="col-6 btn-container pe-2">
-          <button class="btn text-uppercase text-white border-0 px-4 py-3">projects</button>
+          <button class="btn text-uppercase border-0 px-4 py-3">
+            <a class="text-decoration-none">projects
+            </a>
+          </button>
         </div>
         <div class="col-6 btn-container ps-2">
-          <button class="btn text-uppercase text-white border-0 px-4 py-3">skills</button>
+          <button class="btn text-uppercase border-0 px-4 py-3">
+            <a class="text-decoration-none">skills
+            </a>
+          </button>
         </div>
       </div>
     </div>
@@ -22,8 +28,12 @@ export default {
 <style lang="scss" scoped>
 @import "../../../styles/variables.scss";
 
-.nav-container{
+.big-margin-top{
+  margin-top: 6rem;
+}
+.nav-container {
   border-radius: $navRadius;
+  background-color: $secondaryColorBgLight;
 }
 
 .btn-container {
@@ -32,13 +42,20 @@ export default {
     font-size: $normalFontSize;
     font-weight: $smallFontWeight;
     border-radius: $btnRadius;
-    background-color: $primaryColorBrand;
+    background-color: $primaryColorBgLight;
+
+    a {
+      color: $primaryColorBrand;
+    }
   }
 
   &:nth-child(2) {
     button {
-      color: $primaryColorBrand;
       background-color: $secondaryColorBgLight;
+
+      a {
+        color: $primaryColorText;
+      }
     }
   }
 }
