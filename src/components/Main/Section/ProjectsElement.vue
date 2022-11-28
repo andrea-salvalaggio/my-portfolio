a37618daa5f3bc67b7aa0d1a17ecdf964fc76fbd
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-28 10:26:57 +0100

    feat: :sparkles: Create array with projects data

--- src/components/Main/Section/ProjectsElement.vue
+++ src/components/Main/Section/ProjectsElement.vue
@@ -12,16 +12,37 @@
 <script>
 
 export default {
+    data: function(){
+        return{
+            projects:[
+                {
+                    img: require("../../../assets/img/projects/deliveboo.png"),
+                    text: "DeliveBoo Project"
+                },
+                {
+                    img: require("../../../assets/img/projects/boolzapp.png"),
+                    text: "Boolzapp Project"
+                },
+                {
+                    img: require("../../../assets/img/projects/discord.png"),
+                    text: "Discord Project"
+                },
+                {
+                    img: require("../../../assets/img/projects/spotify.png"),
+                    text: "Spotify Project"
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../../../../styles/variables.scss";
 
-.project-container{
-    img{
-        border-radius: $projectRadius;
-    }
+.project-container {
+  img {
+    border-radius: $projectRadius;
+  }
 }
-
 </style>
