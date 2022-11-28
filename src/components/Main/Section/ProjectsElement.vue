dd3525745c2681d591c6558817297cd851724bb0
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-28 10:00:33 +0100

    feat: :sparkles: Create static projects container with images

--- src/components/Main/Section/ProjectsElement.vue
+++ src/components/Main/Section/ProjectsElement.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="row justify-content-center mx-3 mx-md-0">
-        <div class="col-12 d-flex my-5">
-            <img class="w-50" src="../../../assets/img/projects/deliveboo.png" alt="DeliveBoo Project">
+        <div class="col-12 d-flex flex-wrap project-container my-5">
+            <img class="w-50 p-3" src="../../../assets/img/projects/deliveboo.png" alt="DeliveBoo Project">
+            <img class="w-50 p-3" src="../../../assets/img/projects/deliveboo.png" alt="DeliveBoo Project">
+            <img class="w-50 p-3" src="../../../assets/img/projects/deliveboo.png" alt="DeliveBoo Project">
+            <img class="w-50 p-3" src="../../../assets/img/projects/deliveboo.png" alt="DeliveBoo Project">
         </div>
     </div>
 </template>
@@ -12,6 +15,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../../../../styles/variables.scss";
+
+.project-container{
+    img{
+        border-radius: $projectRadius;
+    }
+}
 
 </style>
