a19053730eb16487e273fc6ab077ca8e1c208377
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-28 09:11:34 +0100

    style: :art: Fix container for two button navigation

--- src/components/Main/MainComponent.vue
+++ src/components/Main/MainComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container px-lg-5 mt-5">
-    <div class="row justify-content-center px-md-4 px-lg-5">
-      <div class="col-12 col-md-8 col-lg-6 d-flex justify-content-center bg-dark">
-        <div>
-          <button>projects</button>
+    <div class="row justify-content-center mx-3 mx-md-0">
+      <div class="col-12 col-md-8 col-lg-6 d-flex py-2 bg-dark">
+        <div class="col-6 btn-container pe-2">
+          <button class="btn text-uppercase text-white border-0 px-4 py-3">projects</button>
         </div>
-        <div>
-          <button>skills</button>
+        <div class="col-6 btn-container ps-2">
+          <button class="btn text-uppercase text-white border-0 px-4 py-3">skills</button>
         </div>
       </div>
     </div>
@@ -22,4 +22,20 @@ export default {
 <style lang="scss" scoped>
 @import "../../../styles/variables.scss";
 
+.btn-container {
+  button {
+    width: 100%;
+    font-size: $normalFontSize;
+    font-weight: $smallFontWeight;
+    border-radius: $btnRadius;
+    background-color: $primaryColorBrand;
+  }
+
+  &:nth-child(2) {
+    button {
+      color: $primaryColorBrand;
+      background-color: $secondaryColorBgLight;
+    }
+  }
+}
 </style>
