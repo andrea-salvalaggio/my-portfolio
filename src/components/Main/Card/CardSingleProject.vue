b0c9df28bd086f6354ba8fa08a430d1ac4a44936
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-28 10:55:52 +0100

    feat: :sparkles: Use array to create the projects section

--- src/components/Main/Card/CardSingleProject.vue
+++ src/components/Main/Card/CardSingleProject.vue
@@ -1,18 +1,43 @@
 <template>
-    <div class="d-flex">
-        <p>ciao</p>
-        <p>ciao</p>
-        <p>ciao</p>
-        <p>ciao</p>
+    <div class="col-12 d-flex flex-wrap project-container my-5">
+        <img v-for="(project, index) in projects" :key="index" class="w-50 p-3" :src="project.img" :alt="project.text">
     </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return{
+            projects:[
+                {
+                    img: require("../../../assets/img/projects/deliveboo.png"),
+                    text: "DeliveBoo Project"
+                },
+                {
+                    img: require("../../../assets/img/projects/boolzapp.png"),
+                    text: "Boolzapp Project"
+                },
+                {
+                    img: require("../../../assets/img/projects/discord.png"),
+                    text: "Discord Project"
+                },
+                {
+                    img: require("../../../assets/img/projects/spotify.png"),
+                    text: "Spotify Project"
+                },
+            ]
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../../../../styles/variables.scss";
+
+.project-container {
+  img {
+    border-radius: $projectRadius;
+  }
+}
 
 </style>
--- src/components/Main/Section/ProjectsElement.vue
+++ src/components/Main/Section/ProjectsElement.vue
@@ -1,48 +1,19 @@
 <template>
     <div class="row justify-content-center mx-3 mx-md-0">
-        <div class="col-12 d-flex flex-wrap project-container my-5">
-            <img class="w-50 p-3" src="../../../assets/img/projects/deliveboo.png" alt="DeliveBoo Project">
-            <img class="w-50 p-3" src="../../../assets/img/projects/deliveboo.png" alt="DeliveBoo Project">
-            <img class="w-50 p-3" src="../../../assets/img/projects/deliveboo.png" alt="DeliveBoo Project">
-            <img class="w-50 p-3" src="../../../assets/img/projects/deliveboo.png" alt="DeliveBoo Project">
-        </div>
+        <CardSingleProject/>
     </div>
 </template>
 
 <script>
+import CardSingleProject from '../Card/CardSingleProject.vue';
 
 export default {
-    data: function(){
-        return{
-            projects:[
-                {
-                    img: require("../../../assets/img/projects/deliveboo.png"),
-                    text: "DeliveBoo Project"
-                },
-                {
-                    img: require("../../../assets/img/projects/boolzapp.png"),
-                    text: "Boolzapp Project"
-                },
-                {
-                    img: require("../../../assets/img/projects/discord.png"),
-                    text: "Discord Project"
-                },
-                {
-                    img: require("../../../assets/img/projects/spotify.png"),
-                    text: "Spotify Project"
-                },
-            ]
-        }
+    components: {
+        CardSingleProject
     }
 }
 </script>
 
-<style lang="scss" scoped>
-@import "../../../../styles/variables.scss";
+<style>
 
-.project-container {
-  img {
-    border-radius: $projectRadius;
-  }
-}
 </style>
