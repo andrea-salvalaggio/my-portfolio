1fd5820a9c20601588bf477b25531ca27edae084
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-30 11:00:24 +0100

    style: :art: Add a logo link to homepage in header component

--- src/components/Header/HeaderComponent.vue
+++ src/components/Header/HeaderComponent.vue
@@ -3,7 +3,9 @@
     <div class="row px-md-4 px-lg-5">
       <div class="col-12 d-flex justify-content-between align-items-center py-4 px-5">
         <div class="logo ps-lg-5">
-          <img class="w-50" src="../../assets/img/logo.svg" alt="Personal Logo">
+          <a href="/">
+            <img class="w-50" src="../../assets/img/logo.svg" alt="Personal Logo">
+          </a>
         </div>
         <div class="icon pe-lg-5">
           <i class="ri-moon-line"></i>
