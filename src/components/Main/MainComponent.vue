c45dfce19201e401f83d7afb5f093d282cf71a43
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-30 11:08:04 +0100

    feat: :sparkles: Import skills component in main

--- src/components/Main/MainComponent.vue
+++ src/components/Main/MainComponent.vue
@@ -2,17 +2,20 @@
   <div class="container px-lg-5">
     <NavigationElement/>
     <ProjectsElement/>
+    <SkillsElement/>
   </div>
 </template>
 
 <script>
 import ProjectsElement from './Section/ProjectsElement.vue';
 import NavigationElement from './Section/NavigationElement.vue';
+import SkillsElement from './Section/SkillsElement.vue';
 
 export default {
   components: {
     ProjectsElement,
-    NavigationElement
+    NavigationElement,
+    SkillsElement
   }
 }
 </script>
