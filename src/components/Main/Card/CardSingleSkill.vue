7edae65834a0b2062f75931573d5ea78f02c7d92
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-30 11:28:37 +0100

    feat: :sparkles: Create json for front and back skills

--- src/components/Main/Card/CardSingleSkill.vue
+++ src/components/Main/Card/CardSingleSkill.vue
@@ -1,15 +1,63 @@
 <template>
-    <div>
-
+    <div class="d-flex">
+        
     </div>
 </template>
 
 <script>
 export default {
+    data: function(){
+        return{
+            frontSkills:[
+                {
+                    icon: require("../../../assets/img/icons/html.svg"),
+                    title: "html",
+                },
+                {
+                    icon: require("../../../assets/img/icons/css.svg"),
+                    title: "css",
+                },
+                {
+                    icon: require("../../../assets/img/icons/sass.svg"),
+                    title: "sass",
+                },
+                {
+                    icon: require("../../../assets/img/icons/javascript.svg"),
+                    title: "javascript",
+                },
+                {
+                    icon: require("../../../assets/img/icons/vuejs.svg"),
+                    title: "vuejs",
+                },
+                {
+                    icon: require("../../../assets/img/icons/dart.svg"),
+                    title: "dart",
+                },
+                {
+                    icon: require("../../../assets/img/icons/flutter.svg"),
+                    title: "flutter",
+                },
+            ],
 
+            backSkills:[
+                {
+                    icon: require("../../../assets/img/icons/php.svg"),
+                    title: "php",
+                },
+                {
+                    icon: require("../../../assets/img/icons/laravel.svg"),
+                    title: "laravel",
+                },
+                {
+                    icon: require("../../../assets/img/icons/mysql.svg"),
+                    title: "mysql",
+                },
+            ]
+        }
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import "../../../../styles/variables.scss";
 </style>
