119fa4c4f6a236a693a2c8bdfeb6c1468b4bac4c
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-11-30 09:59:00 +0100

    fix: :zap: Create new component for contact hero section

--- src/components/Header/Hero/HeroComponent.vue
+++ src/components/Header/Hero/HeroComponent.vue
@@ -1,62 +1,22 @@
 <template>
   <div>
     <PresentationElement/>
-
-    <div class="col-12 d-flex justify-content-center text-center pt-2">
-      <div class="btn-container pe-2">
-        <button class="btn btn-shadow text-uppercase text-white border-0 px-4 py-3">
-          <a class="text-decoration-none text-reset" href="https://drive.google.com/file/d/1-1MjQ4YnbzlUrKQYh7FwioFZe9VDRS0g/view?usp=sharing" target="_blank">download cv
-          <i class="ri-download-line align-middle ps-1"></i>
-          </a>
-        </button>
-      </div>
-      <div class="btn-container ps-2">
-        <button class="btn text-uppercase border-0 px-4 py-3">
-          <a class="text-decoration-none text-reset" href="mailto:andrea.salvalaggio95@gmail.com?subject=Richiesta incontro">contact me
-          <i class="ri-mail-line align-top ps-1"></i>
-          </a>
-        </button>
-      </div>
-    </div>
+    <ContactElement/>
   </div>
 </template>
 
 <script>
 import PresentationElement from '../Section/PresentationElement.vue';
+import ContactElement from '../Section/ContactElement.vue';
 
 export default {
   components: {
-    PresentationElement
+    PresentationElement,
+    ContactElement
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@import "../../../../styles/variables.scss";
 
-.btn-container {
-  transition: transform 0.4s ease-in-out;
-
-  button {
-    font-size: $normalFontSize;
-    font-weight: $smallFontWeight;
-    border-radius: $btnRadius;
-    background-color: $primaryColorBrand;
-  }
-
-  &:nth-child(2) {
-    button {
-      color: $primaryColorBrand;
-      background-color: $secondaryColorBgLight;
-    }
-  }
-
-  &:hover{
-    transform: translateY(-0.8rem);
-  }
-}
-
-.btn-shadow {
-  box-shadow: $primaryColorShadowLight;
-}
 </style>
