07409f6446f87884dbab692a6b354c8cf6de5426
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-12-02 11:49:57 +0100

    fix: :sparkles: Fix bug on change dark mode theme

--- src/components/Header/Hero/ToggleComponent.vue
+++ src/components/Header/Hero/ToggleComponent.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="icon pe-lg-5">
-        <i v-if="(mode = !mode)" class="ri-moon-line"
+    <div @click="$emit('toggle')" class="icon pe-lg-5">
+        <!-- <i v-if="(mode = !mode)" class="ri-moon-line"
         @click="$emit('toggle')">
         </i>
         <i v-else class="ri-sun-line"
         @click="$emit('toggle')">
-        </i>
+        </i> -->
     </div>
 </template>
 
@@ -19,9 +19,37 @@ export default {
 @import "@/../styles/variables.scss";
 
 .icon {
+  width: 18px;
+  height: 18px;
+  background-image: url(@/assets/img/icons/dark-mode.svg);
+  background-repeat: no-repeat;
+  background-size: contain;
+
   &:hover {
-    color: $primaryColorBrand;
+    width: 18px;
+    height: 18px;
+    background-image: url(@/assets/img/icons/dark-mode-hover.svg);
+    background-repeat: no-repeat;
+    background-size: contain;
     cursor: pointer;
+  }
+}
+
+.dark .icon {
+  width: 18px;
+  height: 18px;
+  background-image: url(@/assets/img/icons/light-mode.svg);
+  background-repeat: no-repeat;
+  background-size: contain;
+  filter: invert(1);
+
+  &:hover {
+    width: 18px;
+    height: 18px;
+    background-image: url(@/assets/img/icons/light-mode-hover.svg);
+    background-repeat: no-repeat;
+    background-size: contain;
+    filter: none;
   }
 }
 </style>
